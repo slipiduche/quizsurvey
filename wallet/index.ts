2cffeb376205863477491d0e77883e962b104352
slipiduche <alejandrocamacaro91@gmail.com>
2022-04-21 14:10:16 -0400

    showing quiz balance

--- wallet/index.ts
+++ wallet/index.ts
@@ -1,27 +1,30 @@
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import { store } from "../store";
+import { jsonAbi } from "./jsonAbi";
 
 export const verifyNetwork = async () => {
-  if (window.ethereum) {
-    const res = await window.ethereum.request({ method: "eth_chainId" });
+  const web3 = window.ethereum;
+  if (web3) {
+    const res = await web3.request({ method: "eth_chainId" });
     if (res == "0x3") {
       store.wallet.setInvalidChain(false);
     } else {
       store.wallet.setInvalidChain(true);
     }
   } else {
-    // if no window.ethereum then MetaMask is not installed
+    // if no web3 then MetaMask is not installed
     alert(
       "MetaMask is not installed. Please consider installing it: https://metamask.io/download.html"
     );
   }
 };
 export const switchNetwork = async () => {
-  if (window.ethereum) {
+  const web3 = window.ethereum;
+  if (web3) {
     try {
       // check if the chain to connect to is installed
-      console.log("switching to robstein");
-      await window.ethereum.request({
+      console.log("switching to robsten");
+      await web3.request({
         method: "wallet_switchEthereumChain",
         params: [{ chainId: "0x3" }], // chainId must be in hexadecimal numbers
       });
@@ -30,17 +33,18 @@ export const switchNetwork = async () => {
       console.error(error);
     }
   } else {
-    // if no window.ethereum then MetaMask is not installed
+    // if no web3 then MetaMask is not installed
     alert(
       "MetaMask is not installed. Please consider installing it: https://metamask.io/download.html"
     );
   }
 };
 export const connectHandler = async () => {
+  const web3 = window.ethereum;
   console.log("connectHandler");
-  if (window.ethereum) {
+  if (web3) {
     try {
-      const res = await window.ethereum.request({
+      const res = await web3.request({
         method: "eth_requestAccounts",
       });
       console.log(res[0]);
@@ -48,6 +52,7 @@ export const connectHandler = async () => {
       store.wallet.setAccount(res[0]);
       store.wallet.setConnecting(false);
       store.wallet.setConnected(true);
+      getQuizBalance();
     } catch (err) {
       console.error(err);
       store.wallet.setErrorMessage(
@@ -64,6 +69,7 @@ export const connectHandler = async () => {
   }
 };
 export const accountsChanged = async (newAccount: string | string[]) => {
+  const web3 = window.ethereum;
   console.log(newAccount);
   if (typeof newAccount == "string") {
     store.wallet.setAccount(newAccount);
@@ -74,7 +80,7 @@ export const accountsChanged = async (newAccount: string | string[]) => {
   await connectHandler();
 
   try {
-    const balance = await window.ethereum.request({
+    const balance = await web3.request({
       method: "eth_getBalance",
       params: [newAccount.toString(), "latest"],
     });
@@ -90,15 +96,35 @@ export const accountsChanged = async (newAccount: string | string[]) => {
 };
 
 export const chainChanged = (chain: string | string[]) => {
+  const web3 = window.ethereum;
   if (chain == "0x3") {
-    //robstein network
+    //robsten network
     store.wallet.setInvalidChain(false);
   } else {
-    store.wallet.setErrorMessage("Invalid network please switch to robstein");
+    store.wallet.setErrorMessage("Invalid network please switch to robsten");
     store.wallet.setInvalidChain(true);
     store.wallet.setAccount("");
     store.wallet.setBalance("0");
     store.wallet.setConnecting(false);
     store.wallet.setConnected(false);
   }
+};
+
+export const getQuizBalance = async () => {
+  const web3 = window.ethereum;
+  // JSON ABI of the token contract
+  const provider = new ethers.providers.Web3Provider(web3);
+  const contractAddress = "0x74F0B668Ea3053052DEAa5Eedd1815f579f0Ee03"; // address of the token contract
+  const tokenAddress = store.wallet.account; // address of which you want to get the token balance
+
+  const token = new ethers.Contract(contractAddress, jsonAbi, provider);
+  const balance = await token.balanceOf(tokenAddress);
+  // const balance: BigNumber = await token.balanceOf(
+  //   "0x7b45452a0ba54ea3e891e4a6a2fe9adb575e8b69"
+  // );
+
+  const stringBalance = (
+    parseFloat(balance.toString()) / 1000000000000000000
+  ).toString();
+  store.wallet.setQuizBalance(stringBalance);
 };
