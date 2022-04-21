92ee1a0149940156e16c9a1310ac1473976f3cb4
slipiduche <alejandrocamacaro91@gmail.com>
2022-04-21 14:19:37 -0400

    adding sample title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
+import { Col, Row, Typography } from "antd";
 import type { NextPage } from "next";
 import { MainLayout } from "../components";
 
+const { Text, Title } = Typography;
 const Home: NextPage = () => {
   return (
     <>
       <MainLayout>
-        <div style={{ paddingTop: "100px" }}>Home layout</div>
+        <div style={{ paddingTop: "100px" }}>
+          <Row>
+            <Col xs={24}>
+              <Title level={4} style={{textAlign:'center'}}>daily survey</Title>
+            </Col>
+          </Row>
+        </div>
       </MainLayout>
     </>
   );
